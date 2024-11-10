3752402eaad2254d6d38b94e6b33b13437ae2d68
mikegag <michael.gagliardi1@hotmail.com>
2024-11-10 18:31:16 -0500

    added tailwind dependencies

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js}', './public/index.html'],
+  theme: {
+    extend: {
+      colors : {
+          'main-purple': '#6534a7',
+          'dark-purple': '#3f295e',
+          'light purple': '#8762b4',
+          'light grey': '#F4F4F4'
+    }
+  },
+    plugins: [],
+  }
+}
